1bdf273e70a4c7735c947385a6c650e4e721518d
Markus Baumgärtner <baumgaertner@droxit.de>
2019-05-01 13:47:04 +0200

    added license info to src files

--- src/roxconnector.js
+++ src/roxconnector.js
@@ -1,11 +1,28 @@
-//
-// ROXconnector API server package
-// This server exposes a freely configurable REST service for interaction with the core system
-//
-// devs@droxit.de - droxIT GmbH
-//
-// Copyright (c) 2018 droxIT GmbH
-//
+/*
+ * ROXconnector API server package
+ * This server exposes a freely configurable REST service for interaction with the core system
+ *
+ * |------------------- OPEN SOURCE LICENSE DISCLAIMER -------------------|
+ * |                                                                      |
+ * | Copyright (C) 2019  droxIT GmbH - devs@droxit.de                     |
+ * |                                                                      |
+ * | This file is part of ROXconnector.                                   |
+ * |                                                                      |
+ * | ROXconnector is free software: you can redistribute it and/or modify |
+ * | it under the terms of the GNU General Public License as published by |
+ * | the Free Software Foundation, either version 3 of the License, or    |
+ * | (at your option) any later version.                                  |
+ * |                                                                      |
+ * | This program is distributed in the hope that it will be useful,      |
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         |
+ * | GNU General Public License for more details.                         |
+ * |                                                                      |
+ * | You have received a copy of the GNU General Public License           |
+ * | along with this program. See also <http://www.gnu.org/licenses/>.    |
+ * |                                                                      |
+ * |----------------------------------------------------------------------|
+ */
 
 var fs = require('fs');
 var express = require('express');
